cbb8e39095488f0357e192b706c5b3bf68f097c6
Aditya <aditya001tomar@gmail.com>
2020-09-13 23:32:09 +0530

    added css to the search component

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
+import React, {useState, useEffect} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import Button from '@material-ui/core/Button';
 import classes from './SearchBar.module.css';
 
 export default function SearchBar() {
+    const [value, setValue] = useState({handle: "tourist"});
+    const {handle} = value;
+    const onChange = e => setValue({...value, [e.target.name]: e.target.value});
+    const onSubmit = async (e) => {
+      e.preventDefault();
+      console.log(handle);
+    }
+    
+
     return (
-      <div>
+      <div className={classes.container}>
         <div className={classes.margin}>
-          <Grid container spacing={1} alignItems="flex-end">
+        <form onSubmit={e => onSubmit(e)}>
+        <Grid container spacing={1} alignItems="flex-end">
             <Grid item>
               <AccountCircle />
             </Grid>
             <Grid item>
-              <TextField id="input-with-icon-grid" label="Enter cf handle (EX: tourist)" />
+              <TextField id="input-with-icon-grid" value={handle} name="handle" onChange={e => onChange(e)}/>
             </Grid>
+            <Button type="submit" variant="contained" color="primary">Submit</Button>
           </Grid>
+        </form>
         </div>
     </div>
     );
